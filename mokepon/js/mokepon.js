04a056d1411c7c18b2e0a2df96ecda8ede4efac1
Ariel Squatrito <squatritoariel@gmail.com>
2023-09-06 19:03:15 -0300

    Express.js y fetch: API REST con JavaScript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -263,6 +263,18 @@ function iniciarJuego() {
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 
   botonReiniciar.addEventListener("click", reiniciar);
+
+  unirseAlJuego();
+}
+
+function unirseAlJuego() {
+  fetch("http://localhost:8080/unirse").then(function (res) {
+    if (res.ok) {
+      res.text().then(function (respuesta) {
+        console.log(respuesta);
+      });
+    }
+  });
 }
 
 function seleccionarMascotaJugador() {
